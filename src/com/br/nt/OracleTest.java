3e72f8e8d2a5470938861f03d29203e0d5c944e6
Flávio Azevedo <42978944+flavio1407@users.noreply.github.com>
2021-11-17 15:09:17 -0300

    Add files via upload

---
+++ src/com/br/nt/OracleTest.java
@@ -0,0 +1,44 @@
+package com.br.nt;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class OracleTest {
+
+    //https://www.youtube.com/watch?v=-MDPYDi01dM&ab_channel=CodeHelper
+    //https://www.youtube.com/watch?v=bpBLxwWNskM
+
+    public static void  main(String[] args) throws  Exception
+    {
+        // Create jdbc connection obj and load class
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:ORCL","teste","teste");
+
+
+        //create statement obj
+        Statement st= con.createStatement();
+
+        //Execute the query
+        ResultSet rs= st.executeQuery("select * from TESTE_MONITORA WHERE rownum < 53");
+
+        //Print database table records
+        while (rs.next())
+        {
+        //System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+            System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3));
+        }
+
+        //Close all jdbc obj
+        rs.close();
+        st.close();
+        con.close();
+
+
+    }//main
+
+}//Class
+
+
+
